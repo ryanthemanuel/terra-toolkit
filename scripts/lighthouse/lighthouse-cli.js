66aa6b6662933e0705d0c198f45c4f38941a7440
Nathan Faltermeier <nefalt2@gmail.com>
2018-06-27 16:23:51 -0500

    Lighthouse works, accepts multiple urls, passes launch flags to chrome

--- scripts/lighthouse/lighthouse-cli.js
+++ scripts/lighthouse/lighthouse-cli.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const commander = require('commander');
 const lighthouseTest = require('./lighthouse');
+const lighthousePerfConfig = require('lighthouse/lighthouse-core/config/perf.json');
 const loadWebpackConfig = require('../serve/loadWebpackConfig');
 
 const packageJson = require('../../package.json');
@@ -8,56 +9,77 @@ const packageJson = require('../../package.json');
 // Parse process arguments
 commander
   .version(packageJson.version)
-  .option('--config <path>', 'The webpack config to use for serve. Alias for <config>.', undefined)
+  .option('--config <path>', 'The webpack config to use for serve', undefined)
   .option('--port <n>', 'The port the app should listen on', parseInt)
-  .option('--disk', 'The webpack assets will be written to disk instead of a virtual file system.')
-  .option('--page <s>', 'The page to run the lighthouse test on')
+  .option('--disk', 'The webpack assets will be written to disk instead of a virtual file system')
+  .option('--no-headless', 'Chrome will not run in headless mode', false)
+  .option('--chrome-flags <s>', 'Flags to pass to chrome when it is launched (is repeatable)', (flag, flags = []) => {
+    flags.push(flag);
+    return flags;
+  })
+  .option('--pages <s>', 'The pages to run the lighthouse test on (is repeatable)', (page, pages = []) => {
+    pages.push(page);
+    return pages;
+  })
   .option('--perf', 'Lighthouse will only test performance', false)
-  .option('--no-mobile-emulation', 'Lighthouse will not emulate a mobile device', false)
-  .option('--no-cpu-throttle', 'Lighthouse will not throttle the cpu', true)
-  .option('--no-network-throttle', 'Lighthouse will not throttle the network connection', false)
-  .option('--save-assets', 'Lighthouse will save traces and screenshots', false)
   .option('--save-artifacts', 'Lighthouse will save all artifacts', false)
   .parse(process.argv);
 
-if (!commander.page) {
-  console.error('"--page <s>" is a required argument.');
+if (!commander.pages) {
+  console.error('"--pages <s>" is a required argument.');
   process.exit(1);
 }
 
-if (!commander.page) {
+if (!commander.config) {
   console.error('"--config <path>" is a required argument.');
   process.exit(1);
 }
 
 const port = commander.port || process.env.PORT;
 const configPath = loadWebpackConfig(commander.config);
+const chromeFlags = commander.chromeFlags || [];
+if (commander.headless) {
+  chromeFlags.push('--headless');
+}
+const lighthouseConfig = commander.perf ? lighthousePerfConfig : {
+  // Extend Lighthouse's default config
+  extends: true,
+};
 
-lighthouseTest({
-  config: configPath,
-  disk: commander.disk,
-  host: 'localhost',
-  port,
-}, {
-  pages: [commander.page],
-  perf: commander.perf,
-  disableDeviceEmulation: !commander.mobileEmulation,
-  disableCPUThrottling: !commander.cpuThrottle,
-  disableNetworkThrottling: !commander.networkThrottle,
-  saveAssets: commander.saveAssets,
-  saveArtifacts: commander.saveArtifacts,
-}, (returnedResults) => {
-  console.group('\nLighthouse results\n');
+lighthouseTest(
+  {
+    config: configPath,
+    disk: commander.disk,
+    host: 'localhost',
+    port,
+    production: true,
+  },
+  chromeFlags,
+  commander.pages,
+  {
+    gatherMode: commander.saveArtifacts,
+    auditMode: commander.saveArtifacts,
+  },
+  lighthouseConfig,
+  (results) => {
+    results.forEach((result) => {
+      if (result.error) {
+        console.error(`Encountered an error while running lighthouse on page "${result.page}": ${String(result.error)}`);
+        return;
+      }
+      console.group(`\nLighthouse results for ${result.page}\n`);
 
-  console.group('Categories and  scores');
-  returnedResults[0].result.reportCategories.forEach((element) => {
-    console.log(`${element.name}: ${element.score}`);
-  });
-  console.groupEnd('Categories and  scores\n');
+      console.group('Categories and scores');
+      result.result.reportCategories.forEach((element) => {
+        console.log(`${element.name}: ${element.score}`);
+      });
+      console.groupEnd('Categories and scores');
 
-  console.log('\n');
-  console.log(`First meaningful paint: ${returnedResults[0].result.audits['first-meaningful-paint'].displayValue}`);
-  console.log(`First Interactive: ${returnedResults[0].result.audits['first-interactive'].displayValue}`);
+      console.log('\n');
+      console.log(`First meaningful paint: ${result.result.audits['first-meaningful-paint'].displayValue}`);
+      console.log(`First Interactive: ${result.result.audits['first-interactive'].displayValue}`);
 
-  console.groupEnd('Lighthouse results');
-});
+      console.groupEnd(`\nLighthouse results for ${result}\n`);
+    });
+  },
+);
--- scripts/lighthouse/lighthouse.js
+++ scripts/lighthouse/lighthouse.js
@@ -4,45 +4,65 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 let server;
-let chrome;
-let superResults;
 
-const onBuildFinished = (lighthouseOptions) => {
-  console.log('Launching headless chrome');
-  return chromeLauncher.launch({ chromeFlags: ['--headless'] }).then((chromeArg) => {
-    chrome = chromeArg;
+// Called when serve-static finishes building and starts serving
+const onBuildFinished = (chromeFlags, pages, lighthouseFlags, lighthouseConfig, finishedCallback) => {
+  console.log('Launching chrome');
+  return chromeLauncher.launch({ chromeFlags }).then(async (chrome) => {
     const port = chrome.port;
+
     // Need to give lighthouse chrome's debugging port
-    const lighthouseArgs = { ...lighthouseOptions, port };
+    const newLighthouseFlags = { ...lighthouseFlags, port };
     console.log('Starting lighthouse');
-    let results = [];
-    lighthouseOptions.pages.forEach((page) => {
-      lighthouse(page, lighthouseArgs, null).then((lighthouseResult) => {
-        results = results.push({ page, result: lighthouseResult });
-      });
-    });
-    console.log('lighthouse done');
+
+    const results = [];
+    /* eslint-disable no-await-in-loop */
+    // We're running performance tests so we should wait for each test to finish before starting the next
+    for (let i = 0; i < pages.length; i += 1) {
+      try {
+        const lighthouseResult = await lighthouse(pages[i], newLighthouseFlags, lighthouseConfig);
+        results.push({ page: pages[i], result: lighthouseResult });
+      } catch (error) {
+        results.push({ page: pages[i], error });
+      }
+    }
+    /* eslint-enable no-await-in-loop */
+
+    console.log(`Lighthouse finished${pages.length > 1 && ' all pages'}`);
     chrome.kill();
     server.close();
-    superResults = results;
+    finishedCallback(results);
+  }, (error) => {
+    if (server) {
+      server.close();
+    }
+
+    throw error;
   });
 };
 
-const lighthouseTest = (options, lighthouseOptions, finishedCallback) => {
+// If our lighthouse dependency gets updated we need to update the links
+/**
+ * @param options Options to be passed to static-serve
+ * @param chromeFlags Flags to pass to chrome at startup
+ * @param pages An array of urls to test with lighthouse
+ * @param lighthouseFlags Flags to be passed to lighthouse. See:
+ *   https://github.com/GoogleChrome/lighthouse/blob/v2.9.4/typings/externs.d.ts#L9
+ * @param lighthouseConfig Config options to be passed to lighthouse. See:
+ *   https://github.com/GoogleChrome/lighthouse/blob/v2.9.4/docs/configuration.md
+ * @param finishedCallback Callback for when all pages have been tested with lighthouse
+ * @return An object with a page property for which page was tested and either a results property or an error property
+ */
+const lighthouseTest = (options, chromeFlags, pages, lighthouseFlags, lighthouseConfig, finishedCallback) => {
   const args = {
     ...options,
-    callback: onBuildFinished.bind(this, lighthouseOptions),
+    callback: onBuildFinished.bind(this, chromeFlags, pages, lighthouseFlags, lighthouseConfig, finishedCallback),
   };
   server = serve(args).then((serverArg) => {
+    // Allows the server to be closed when lighthouse finishes or chrome errors
     server = serverArg;
-    server.on('close', () => {
-      finishedCallback(superResults);
-    });
-  }, () => {
-    // Kill chrome if webpack gets an error
-    if (chrome) {
-      chrome.kill();
-    }
+  }, (error) => {
+    throw error;
   });
 };
 
