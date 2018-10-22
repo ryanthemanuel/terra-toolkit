c4b2ac69c1186e9a077a56fb6ee45c8ffaa7be2f
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-10-22 14:17:33 -0500

    fix space issue for cmd line args

--- scripts/utils/parse-cli-list.js
+++ scripts/utils/parse-cli-list.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-useless-escape
-const parseCLIList = list => list.replace(/[\[\'\'\]\s]/g, '').split(',').map(String);
+const parseCLIList = list => list.replace(/[\[\'\'\]\s], '').split(',').map(String);
 
 module.exports = parseCLIList;
